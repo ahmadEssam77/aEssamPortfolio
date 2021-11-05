1a907775ce9240d8ac470b7bfb7f1cfeb5c1655a
ahmadEssam77 <ahmad77essam@gmail.com>
2021-11-05 08:23:22 +0200

    finishing home section with its media query

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// const getTheBody = document.getElementById("getBody");
+const homePage = document.getElementById("home"); // Select home page
+
+// Display the home page none. Then show it again by slideDown jquery effect
+homePage.style.display = "none";
+$("#home").slideDown(850);
